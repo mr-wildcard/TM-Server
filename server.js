ecd1260576f9dd7dcefd54b0cb032049252d6360
Julien Viala <dev.vialajulien@gmail.com>
2015-09-01 14:22:59 +0200

    use Twit package for twitter API

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 "use strict";
 
+let credentials = require('./credentials');
 let koa = require('koa');
 let app = koa();
+let Twit = require('twit');
+let twitter = new Twit(credentials.twitter);
 
 // logger
 app.use(function *(next) {
@@ -11,6 +14,11 @@ app.use(function *(next) {
     console.log('%s %s - %s', this.method, this.url, ms);
 });
 
+var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ];
+var stream = twitter.stream('statuses/filter', { locations: sanFrancisco });
+
+stream.on('tweet', function (tweet) {
+
+});
 
 app.listen(3000);
-
