3df6f54cbd1609bdd9499c7fc98343c27a5effe4
Julien Viala <dev.vialajulien@gmail.com>
2015-09-03 18:27:22 +0200

    listen to rethinkdb changes on Tweet model

--- server.js
+++ server.js
@@ -4,19 +4,25 @@ let http = require('http');
 let koa = require('koa');
 let app = koa();
 let socketio = require('socket.io');
-let thinky = require('thinky')();
+let thinky = require('thinky')({
+    db: 'tm'
+});
 let type = thinky.type;
 let credentials = require('./credentials');
 let Twit = require('twit');
 let twitter = new Twit(credentials.twitter);
 
 let Tweet = thinky.createModel('Tweet', {
-    id: type.number(),
+    tweetId: type.number(),
     authorName: type.string(),
     authorScreenName: type.string(),
     content: type.string(),
     avatarUrl: type.string(),
-    validated: type.boolean()
+    validated: type.boolean().default(false)
+});
+
+Tweet.docAddListener('save', function(tweet) {
+    console.log("Table "+tweet+" is ready");
 });
 
 // logger
@@ -27,34 +33,65 @@ app.use(function *(next) {
     console.log('%s %s - %s', this.method, this.url, ms);
 });
 
+Tweet.changes().then(function(feed) {
+    feed.each(function(error, tweet) {
+        if (error) {
+            console.log(error);
+            process.exit(1);
+        }
+
+        if (tweet.isSaved() === false) {
+            console.log("The following document was deleted:");
+            console.log(stringify(tweet.getOldValue()));
+        }
+        else if (tweet.getOldValue() == null) {
+            //console.log("A new tweet was inserted: " + tweet.id);
+            io.emit('tweet:new', tweet);
+        }
+        else {
+            console.log("A document was updated.");
+            console.log("Old value:");
+            console.log(stringify(tweet.getOldValue()));
+            console.log("New value:");
+            console.log(stringify(tweet));
+        }
+    });
+}).error(function(error) {
+    console.log(error);
+    process.exit(1);
+});
+
+var stringify = function(doc) {
+    return JSON.stringify(doc, null, 2);
+};
+
+
 let server = http.createServer(app.callback());
 let io = socketio(server);
 
 server.listen(3000);
 
-io.on('connection', socket => {
+var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ];
+var stream = twitter.stream('statuses/filter', { locations: sanFrancisco });
+stream.on('tweet', tweet => {
 
-    var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ];
-    var stream = twitter.stream('statuses/filter', { locations: sanFrancisco });
-
-    stream.on('tweet', tweet => {
-
-        let _tweet = new Tweet({
-            id: tweet.id,
-            authorName: tweet.user.name,
-            authorScreenName: tweet.user.screen_name,
-            content: tweet.text,
-            avatarUrl: tweet.user.profile_image_url,
-            validated: false
-        });
-
-        _tweet
-            .save()
-            .then(result => {
-                console.log('tweet ' + result.id + ' successfully saved.');
-
-                socket.emit('new_tweet', result);
-            })
-            .error(error => console.log('an error occured while saving tweet: ' + error))
+    let _tweet = new Tweet({
+        tweetId: tweet.id,
+        authorName: tweet.user.name,
+        authorScreenName: tweet.user.screen_name,
+        content: tweet.text,
+        avatarUrl: tweet.user.profile_image_url
     });
+
+    _tweet
+        .save()
+        .error(error => console.log('an error occured while saving tweet: ' + error));
+});
+
+io.on('connection', socket => {
+    console.log('Client Socket.IO connected...');
+});
+
+io.on('tweet:accept:start', tweet_id => {
+    console.log('accepting tweet ' + tweet_id);
 });
